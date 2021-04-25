f128f85773622c53f468590a1c2fd95b142bccd7
zhekaig2 <zhekaig2@illinois.edu>
2021-04-25 17:09:16 -0500

    Dijkstra

--- main.cpp
+++ main.cpp
@@ -10,19 +10,17 @@ using std::endl;
 int main() {
   // how to use getAjacentRoute, pass a code of an airport
 
-  OpenFlight o2("data/airports.csv", "data/routes.csv", "2965");
-  // OpenFlight o2("data/airports.csv", "data/routes.csv", "1");
-  //for (unsigned i = 0; i < o2.getAdjacentRoute(o2.getStart().getID()).size(); i++) {
-    //cout << o2.getAdjacentRoute(o2.getStart().getID())[i].getDest().getID() << ", "<< o2.getAdjacentRoute(o2.getStart().getID())[i].getDistance() << "; ";
-  //}
+  OpenFlight o2("data/simple_airports_3.csv", "data/simple_routes_3.csv", "1064");
+  for (unsigned i = 0; i < o2.getAdjacentRoute(o2.getStart().getID()).size(); i++) {
+    cout << o2.getAdjacentRoute(o2.getStart().getID())[i].getDest().getID() << ", "<< o2.getAdjacentRoute(o2.getStart().getID())[i].getDistance() << "; ";
+  }
   cout << "finish" << endl;
-  Dijkstra d(o2, o2.getAirport("3400"));
-  cout << "finish constructing" << endl;
+  Dijkstra d(o2, o2.getAirport("1550"));
   cout << d.get_distance() << endl;
-  //cout << d.get_distance(o2.getAirport("2962")) << endl;
-  //cout << d.get_distance(o2.getAirport("2966")) << endl;
-  //Dijkstra c(o2, o2.getAirport("1"));
-  //cout << c.get_distance(o2.getAirport("2")) << endl;
+  Dijkstra c(o2, o2.getAirport("1064"));
+  cout << c.get_distance() << endl;
+  Dijkstra b(o2, o2.getAirport("302"));
+  cout << b.get_distance() << endl;
   for (unsigned i = 0; i < d.get_path().size(); i++) {
     cout << d.get_path()[i].getID() << ", ";
   }
