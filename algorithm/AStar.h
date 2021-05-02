2f6db50c5b5442b4097c8afd29bf139a3c43465d
liwens3 <liwens3@illinois.edu>
2021-05-03 07:02:08 +0800

    Add more documentation

--- algorithm/AStar.h
+++ algorithm/AStar.h
@@ -8,24 +8,51 @@ using std::priority_queue;
 
 class AStar {
     public:
+        /**
+         * get the path of all airports during A* search
+         * @param  graph the OpenFlight graph for A* search
+         * @param  src starting airport
+         * @param  dest ending airport
+         * @return {vector<Airport>}
+         */
         vector<Airport> reconstructPath(OpenFlight graph, Airport src, Airport dest);
-
     private:
+        /**
+         * construct the path from starting airport to ending airport
+         * @param  graph the OpenFlight graph for A* search
+         * @param  src starting airport
+         * @param  dest ending airport
+         * @return {Airport}
+         */
         Airport aStarSearch(OpenFlight graph, Airport src, Airport dest);
-        // Comparator for priority_queue
+        /**
+         * Comparator for priority_queue (open_set)
+         */
         struct Comparator {
             bool operator()(const pair<Airport, double> &lhs,
                             const pair<Airport, double> &rhs) {
               return lhs.second > rhs.second;
             }
         };
-        // we need to check whether nearby airport has already been in the open_set
+        /**
+         * we need to check whether nearby airport has already been in the open_set
+         * @param  temp_set the copy of open_set
+         * @param  nearby nearby airport
+         * @return {bool}
+         */
         bool isExist(priority_queue<pair<Airport, double>,
                                     vector<pair<Airport, double>>, Comparator>
                          temp_set,
                      const Airport &nearby) const;
-        // heuristic function
+        /**
+         * heuristic function
+         * @param  src starting airport
+         * @param  dest ending airport
+         * @return {double}
+         */
         double heuristic(const Airport &src, const Airport &dest) const;
+
+
         // We use this queue to find the minest f
         priority_queue<pair<Airport, double>, vector<pair<Airport, double>>,
                        Comparator>
