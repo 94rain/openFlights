6f1aa8a6e55e16584a1db1e90f73ca1986b57ca2
Shuyang Ji <sji15@illinois.edu>
2021-05-02 21:19:44 +0800

    fix indentation error in autoformat

--- algorithm/AStar.cpp
+++ algorithm/AStar.cpp
@@ -4,55 +4,55 @@ using std::cout;
 using std::endl;
 
 Airport AStar::aStarSearch(OpenFlight graph, Airport src, Airport dest) {
-  pre.clear();
-  // initialize f and g with default value of infinity
-  for (auto i : graph.getAirport()) {
-    f[i.first] = DBL_MAX;
-    g[i.first] = DBL_MAX;
-  }
-  g[src.getID()] = 0;
-  f[src.getID()] = heuristic(src, dest);
-  open_set.push(make_pair(src, f[src.getID()]));
-  while (!open_set.empty()) {
-    // we need to find smallest fscore
-    Airport current = open_set.top().first;
-    string currentID = current.getID();
-    // if current node is destination, we finished our search
-    if (current.getID() == dest.getID()) {
-      return current;
+    pre.clear();
+    // initialize f and g with default value of infinity
+    for (auto i : graph.getAirport()) {
+      f[i.first] = DBL_MAX;
+      g[i.first] = DBL_MAX;
     }
-    open_set.pop();
-    vector<Route> routes = graph.getAdjacentRoute(currentID);
-    for (auto route : routes) {
-      const Airport &nearby = route.getDest();
-      if (g[currentID] + route.getDistance() < g[nearby.getID()]) {
-        // This path is cheaper than any previous one.
-        // Thus we need to update g and f
-        pre[nearby.getID()] = current;
-        g[nearby.getID()] = g[currentID] + route.getDistance();
-        f[nearby.getID()] = g[nearby.getID()] + heuristic(nearby, dest);
-        // we use this to check whether nearby is in open_set
-        if (!isExist(open_set, nearby)) {
-          open_set.push(make_pair(nearby, f[nearby.getID()]));
+    g[src.getID()] = 0;
+    f[src.getID()] = heuristic(src, dest);
+    open_set.push(make_pair(src, f[src.getID()]));
+    while (!open_set.empty()) {
+      // we need to find smallest fscore
+      Airport current = open_set.top().first;
+      string currentID = current.getID();
+      // if current node is destination, we finished our search
+      if (current.getID() == dest.getID()) {
+        return current;
+      }
+      open_set.pop();
+      vector<Route> routes = graph.getAdjacentRoute(currentID);
+      for (auto route : routes) {
+        const Airport &nearby = route.getDest();
+        if (g[currentID] + route.getDistance() < g[nearby.getID()]) {
+          // This path is cheaper than any previous one.
+          // Thus we need to update g and f
+          pre[nearby.getID()] = current;
+          g[nearby.getID()] = g[currentID] + route.getDistance();
+          f[nearby.getID()] = g[nearby.getID()] + heuristic(nearby, dest);
+          // we use this to check whether nearby is in open_set
+          if (!isExist(open_set, nearby)) {
+            open_set.push(make_pair(nearby, f[nearby.getID()]));
+          }
         }
       }
     }
-  }
-  // we did not found the destination airport
-  return Airport();
+    // we did not found the destination airport
+    return Airport();
 }
 
 // we need to check whether nearby airport has already been in the open_set
 bool AStar::isExist(priority_queue<pair<Airport, double>, vector<pair<Airport, double>>, Comparator>
                         temp_set, const Airport &nearby) const {
-  while (!temp_set.empty()) {
-    const Airport &temp_airport = temp_set.top().first;
-    if (temp_airport.getID() == nearby.getID()) {
-      return true;
+    while (!temp_set.empty()) {
+      const Airport &temp_airport = temp_set.top().first;
+      if (temp_airport.getID() == nearby.getID()) {
+        return true;
+      }
+      temp_set.pop();
     }
-    temp_set.pop();
-  }
-  return false;
+    return false;
 }
 
 // Distance for the heuristic function
--- tests/test_AStar.cpp
+++ tests/test_AStar.cpp
@@ -90,20 +90,6 @@ TEST_CASE("Astar dataset sample #3 case #2 check", "[astar][dataset=3]") {
 
 TEST_CASE("Astar dataset sample #4 case #1 check", "[astar][dataset=4]") {
 
-  OpenFlight o1("data/simple_airports_4.csv", "data/simple_routes_4.csv", "1154");
-  AStar a1;
-  Airport start = o1.getStart();
-  Airport end = o1.getAirport("344");
-  vector<Airport> path = a1.reconstructPath(o1, start, end);
-  REQUIRE(path.size() == 5);
-  vector<string> actual_path{"1154", "1074", "1128", "1555", "344"};
-    for (unsigned i = 0; i < path.size(); i++) {
-      REQUIRE(path[i].getID() == actual_path[i]);
-    }
-}
-
-TEST_CASE("Astar dataset sample #4 case #2 check", "[astar][dataset=4]") {
-
   OpenFlight o1("data/simple_airports_4.csv", "data/simple_routes_4.csv", "1128");
   AStar a1;
   Airport start = o1.getStart();
@@ -111,6 +97,20 @@ TEST_CASE("Astar dataset sample #4 case #2 check", "[astar][dataset=4]") {
   vector<Airport> path = a1.reconstructPath(o1, start, end);
   REQUIRE(path.size() == 3);
   vector<string> actual_path{"1128", "1074", "1154"};
+    for (unsigned i = 0; i < path.size(); i++) {
+      REQUIRE(path[i].getID() == actual_path[i]);
+    }
+}
+
+TEST_CASE("Astar dataset sample #4 case #2 check", "[astar][dataset=4]") {
+
+  OpenFlight o1("data/simple_airports_4.csv", "data/simple_routes_4.csv", "1154");
+  AStar a1;
+  Airport start = o1.getStart();
+  Airport end = o1.getAirport("344");
+  vector<Airport> path = a1.reconstructPath(o1, start, end);
+  REQUIRE(path.size() == 5);
+  vector<string> actual_path{"1154", "1074", "1128", "1555", "344"};
     for (unsigned i = 0; i < path.size(); i++) {
       REQUIRE(path[i].getID() == actual_path[i]);
     }
