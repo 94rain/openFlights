cef4761d4a5d25e8ea7f269a922be0d68023a686
Shuyang Ji <sji15@illinois.edu>
2021-05-04 01:07:22 +0800

    put start/end/path output in if-else clause

--- main.cpp
+++ main.cpp
@@ -14,9 +14,7 @@ using std::string;
 int main() {
   string str;
   while (true) {
-    cout << "Select your algorithm from: (enter a number from 1, 2, 3 or exit) \n 1. BFS "
-          "\n 2. Dijkstra \n 3. A* "
-      << endl;
+    cout << "Select your algorithm from: (enter a number from 1, 2, 3 or exit) \n 1. BFS \n 2. Dijkstra \n 3. A* " << endl;
     getline(cin, str);
     if (str == "1") {
       OpenFlight o("data/airports.csv", "data/routes.csv", "1");
@@ -25,58 +23,59 @@ int main() {
       for (auto i : path) {
         cout << i.getName() << endl;
       }
-      cout<< "BFS ends" <<endl;
-      cout<< "The total number of Airport is " << s.getCount() << endl;
+      cout << "BFS ends" << endl;
+      cout << "The total number of Airport is " << s.getCount() << endl;
     } else if (str == "2") {
       string src;
       string dest;
-      cout<<"please enter the ID of starting airport: "<<endl;
+      cout << "please enter the ID of starting airport: " << endl;
       getline(cin, src);
-      cout<<"please enter the ID of ending airport: "<<endl; 
+      cout << "please enter the ID of ending airport: " << endl;
       getline(cin, dest);
       OpenFlight o("data/airports.csv", "data/routes.csv", src);
       Dijkstra d(o);
       Airport start = o.getStart();
       Airport end = o.getAirport(dest);
       vector<Airport> path = d.get_path(end);
-      cout << "Starting Airport is " << start.getName() << endl;
-      cout << "Ending Airport is " << end.getName() << endl;
-      cout << "Dijkstra path: " << endl;
       if (path.empty()) {
-        cout << "Error, there is no route between source airport and destination airport" << endl;
+        cout << "Error, there is no route between source airport and "
+                "destination airport"
+             << endl;
       } else {
+        cout << "Starting Airport is " << start.getName() << endl;
+        cout << "Ending Airport is " << end.getName() << endl;
+        cout << "Dijkstra path: " << endl;
         for (auto airport : path) {
-          cout << airport.getID() << ":" << "\t" << airport.getName() << endl;
+          cout << airport.getID() << ":\t" << airport.getName() << endl;
         }
       }
     } else if (str == "3") {
       string src;
       string dest;
-      cout<<"please enter the ID of starting airport: "<<endl;
+      cout << "please enter the ID of starting airport: " << endl;
       getline(cin, src);
-      cout<<"please enter the ID of ending airport: "<<endl; 
+      cout << "please enter the ID of ending airport: " << endl;
       getline(cin, dest);
       OpenFlight o("data/airports.csv", "data/routes.csv", src);
       AStar a;
       Airport start = o.getStart();
       Airport end = o.getAirport(dest);
       vector<Airport> path = a.reconstructPath(o, start, end);
-      cout << "Starting Airport is " << start.getName() << endl;
-      cout << "Ending Airport is " << end.getName() << endl;
-      cout << "A* search path: " << endl;
       if (path[0] == Airport()) {
         cout << "Error, there is no route between source airport and destination airport" << endl;
       } else {
+        cout << "Starting Airport is " << start.getName() << endl;
+        cout << "Ending Airport is " << end.getName() << endl;
+        cout << "A* search path: " << endl;
         for (auto airport : path) {
-          cout << airport.getID() << ":" << "\t" << airport.getName() << endl;
+          cout << airport.getID() << ":\t" << airport.getName() << endl;
         }
       }
-    } else if(str == "exit"){
-      cout<<"Program ends"<<endl;
+    } else if (str == "exit") {
+      cout << "Program ends" << endl;
       break;
     } else {
       cout << "Your input is invalid. Please try again. (enter a number from 1, 2, 3 or exit)" << endl;
     }
   }
-
 }
