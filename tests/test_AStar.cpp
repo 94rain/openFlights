a6b5fd4869f9cdb581cb438df92ba1d468e4bee4
liwens3 <liwens3@illinois.edu>
2021-05-05 06:01:26 +0800

    Modify small bugs

--- tests/test_AStar.cpp
+++ tests/test_AStar.cpp
@@ -39,8 +39,8 @@ TEST_CASE("Astar dataset sample #2 case #1 check", "[astar][dataset=2]") {
   Airport start = o1.getStart();
   Airport end = o1.getAirport("2965");
   vector<Airport> path = a1.reconstructPath(o1, start, end);
-  REQUIRE(path.size() == 5);
-  vector<string> actual_path{"2922", "6969", "2975", "2990", "2965"};
+  REQUIRE(path.size() == 4);
+  vector<string> actual_path{"2922", "6969", "2990", "2965"};
     for (unsigned i = 0; i < path.size(); i++) {
       REQUIRE(path[i].getID() == actual_path[i]);
     }
@@ -48,13 +48,13 @@ TEST_CASE("Astar dataset sample #2 case #1 check", "[astar][dataset=2]") {
 
 TEST_CASE("Astar dataset sample #2 case #2 check", "[astar][dataset=2]") {
 
-  OpenFlight o1("data/simple_airports_2.csv", "data/simple_routes_2.csv", "6156");
+  OpenFlight o1("data/simple_airports_2.csv", "data/simple_routes_2.csv", "2979");
   AStar a1;
   Airport start = o1.getStart();
   Airport end = o1.getAirport("2922");
   vector<Airport> path = a1.reconstructPath(o1, start, end);
   REQUIRE(path.size() == 5);
-  vector<string> actual_path{"6156" ,"2990" ,"2975" ,"6969" ,"2922"};
+  vector<string> actual_path{"2979" ,"2965" ,"2990" ,"6969" ,"2922"};
     for (unsigned i = 0; i < path.size(); i++) {
       REQUIRE(path[i].getID() == actual_path[i]);
     }
